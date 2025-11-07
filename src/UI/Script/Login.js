e23556687a3e028711fd556aad8f7fb442f7e7b6
Rain-Hub1 <rainhubsla@gmail.com>
2025-11-07 15:28:21 -0300

    Delete src/UI directory

--- src/UI/Script/Login.js
+++
@@ -1,9 +0,0 @@
-const loginForm = document.getElementById('login-form');
-
-if (loginForm) {
-    loginForm.addEventListener('submit', function(event) {
-        event.preventDefault();
-        console.log('Tentativa de login...');
-        alert('Funcionalidade de login ainda não implementada.');
-    });
-}
--- src/UI/Script/Register.js
+++
@@ -1,9 +0,0 @@
-const registerForm = document.getElementById('register-form');
-
-if (registerForm) {
-    registerForm.addEventListener('submit', function(event) {
-        event.preventDefault();
-        console.log('Tentativa de registro...');
-        alert('Funcionalidade de registro ainda não implementada.');
-    });
-}
