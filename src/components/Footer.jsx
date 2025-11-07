1756a51480110db34a8b61ef4bd76a3244f4f59c
Rain-Hub1 <rainhubsla@gmail.com>
2025-11-07 15:32:38 -0300

    Create Footer.jsx

---
+++ src/components/Footer.jsx
@@ -0,0 +1,11 @@
+import './Footer.css';
+
+const Footer = () => {
+    return (
+        <footer className="main-footer">
+            <p>&copy; 2025 Manus IA. Todos os direitos reservados.</p>
+        </footer>
+    );
+};
+
+export default Footer;
