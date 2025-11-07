dfa04cf669b420f70e601573f7daf583eea8ed1b
Rain-Hub1 <rainhubsla@gmail.com>
2025-11-07 15:43:41 -0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,24 @@
-import { HashRouter, Routes, Route } from 'react-router-dom';
+import { HashRouter, Routes, Route, Navigate } from 'react-router-dom';
 import MainLayout from './layouts/MainLayout';
+import SettingsLayout from './layouts/SettingsLayout';
 import Home from './pages/Home';
 import Login from './pages/Login';
 import Register from './pages/Register';
-import Settings from './pages/Settings';
+import { ProfileSettings, AccountSettings } from './pages/Settings';
 
 function App() {
   return (
     <HashRouter>
       <Routes>
-        <Route element={<MainLayout />}>
-          <Route path="/" element={<Home />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/register" element={<Register />} />
-          <Route path="/settings/*" element={<Settings />} />
+        <Route path="/" element={<MainLayout />}>
+          <Route index element={<Home />} />
+          <Route path="login" element={<Login />} />
+          <Route path="register" element={<Register />} />
+          <Route path="settings" element={<SettingsLayout />}>
+            <Route index element={<Navigate to="profile" replace />} />
+            <Route path="profile" element={<ProfileSettings />} />
+            <Route path="account" element={<AccountSettings />} />
+          </Route>
         </Route>
       </Routes>
     </HashRouter>
